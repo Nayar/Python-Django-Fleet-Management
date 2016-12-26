3ff78e4b58bd34492535bd42a1f70448f519d658
Sérgio C. N. Cruz <sergiocncruz@gmail.com>
2016-12-26 14:33:05 -0300

    Set verbose name in the models

--- core/models.py
+++ core/models.py
@@ -12,74 +12,10 @@ class Agencies(models.Model):
     title = models.CharField(max_length=160, blank=True, null=True)
 
     class Meta:
+        verbose_name = 'Agency'
+        verbose_name_plural = 'Agencies'
         managed = False
         db_table = 'agencies'
-
-
-class AuthGroup(models.Model):
-    name = models.CharField(unique=True, max_length=80)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group'
-
-
-class AuthGroupPermissions(models.Model):
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group_permissions'
-        unique_together = (('group', 'permission'),)
-
-
-class AuthPermission(models.Model):
-    name = models.CharField(max_length=255)
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
-    codename = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_permission'
-        unique_together = (('content_type', 'codename'),)
-
-
-class AuthUser(models.Model):
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.IntegerField()
-    username = models.CharField(unique=True, max_length=150)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.CharField(max_length=254)
-    is_staff = models.IntegerField()
-    is_active = models.IntegerField()
-    date_joined = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user'
-
-
-class AuthUserGroups(models.Model):
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_groups'
-        unique_together = (('user', 'group'),)
-
-
-class AuthUserUserPermissions(models.Model):
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_user_permissions'
-        unique_together = (('user', 'permission'),)
 
 
 class City(models.Model):
@@ -87,52 +23,10 @@ class City(models.Model):
     state = models.ForeignKey('States', models.DO_NOTHING, db_column='state', blank=True, null=True)
 
     class Meta:
+        verbose_name = 'City'
+        verbose_name_plural = 'Cities'
         managed = False
         db_table = 'city'
-
-
-class DjangoAdminLog(models.Model):
-    action_time = models.DateTimeField()
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.SmallIntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'django_admin_log'
-
-
-class DjangoContentType(models.Model):
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'django_content_type'
-        unique_together = (('app_label', 'model'),)
-
-
-class DjangoMigrations(models.Model):
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_migrations'
-
-
-class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_session'
 
 
 class Drivers(models.Model):
@@ -140,6 +34,8 @@ class Drivers(models.Model):
     sector = models.ForeignKey('Sectors', models.DO_NOTHING, db_column='sector', blank=True, null=True)
 
     class Meta:
+        verbose_name = 'Driver'
+        verbose_name_plural = 'Drivers'
         managed = False
         db_table = 'drivers'
 
@@ -149,6 +45,8 @@ class FuelStations(models.Model):
     city = models.ForeignKey(City, models.DO_NOTHING, db_column='city', blank=True, null=True)
 
     class Meta:
+        verbose_name = 'Fuel Station'
+        verbose_name_plural = 'Fuel Stations'
         managed = False
         db_table = 'fuel_stations'
 
@@ -157,6 +55,8 @@ class Fuels(models.Model):
     name = models.CharField(max_length=160, blank=True, null=True)
 
     class Meta:
+        verbose_name = 'Fuel'
+        verbose_name_plural = 'Fuel'
         managed = False
         db_table = 'fuels'
 
@@ -169,6 +69,8 @@ class Sectors(models.Model):
     agency = models.ForeignKey(Agencies, models.DO_NOTHING, db_column='agency', blank=True, null=True)
 
     class Meta:
+        verbose_name = 'Sector'
+        verbose_name_plural = 'Sectors'
         managed = False
         db_table = 'sectors'
 
@@ -182,6 +84,8 @@ class States(models.Model):
     country = models.IntegerField(blank=True, null=True)
 
     class Meta:
+        verbose_name = 'State'
+        verbose_name_plural = 'States'
         managed = False
         db_table = 'states'
 
@@ -195,6 +99,8 @@ class Supplies(models.Model):
     auto_date = models.DateTimeField()
 
     class Meta:
+        verbose_name = 'Supply'
+        verbose_name_plural = 'Supplies'
         managed = False
         db_table = 'supplies'
 
@@ -206,6 +112,8 @@ class Travels(models.Model):
     vehicle = models.ForeignKey('Vehicles', models.DO_NOTHING, db_column='vehicle', blank=True, null=True)
 
     class Meta:
+        verbose_name = 'Travel'
+        verbose_name_plural = 'Travels'
         managed = False
         db_table = 'travels'
 
@@ -216,6 +124,8 @@ class Users(models.Model):
     password = models.CharField(max_length=160)
 
     class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
         managed = False
         db_table = 'users'
 
@@ -240,6 +150,8 @@ class Vehicles(models.Model):
     provenance = models.ForeignKey('VehiclesProvenances', models.DO_NOTHING, db_column='provenance', blank=True, null=True)
 
     class Meta:
+        verbose_name = 'Vehicle'
+        verbose_name_plural = 'Vehicles'
         managed = False
         db_table = 'vehicles'
 
@@ -248,6 +160,8 @@ class VehiclesBrands(models.Model):
     name = models.CharField(max_length=160, blank=True, null=True)
 
     class Meta:
+        verbose_name = 'Vehicle Brand'
+        verbose_name_plural = 'Vehicle Brands'
         managed = False
         db_table = 'vehicles_brands'
 
@@ -260,6 +174,8 @@ class VehiclesModels(models.Model):
     brand = models.ForeignKey(VehiclesBrands, models.DO_NOTHING, db_column='brand', blank=True, null=True)
 
     class Meta:
+        verbose_name = 'Vehicle Model'
+        verbose_name_plural = 'Vehicle Models'
         managed = False
         db_table = 'vehicles_models'
 
@@ -271,6 +187,8 @@ class VehiclesProvenances(models.Model):
     name = models.CharField(max_length=160, blank=True, null=True)
 
     class Meta:
+        verbose_name = 'Vehicle Provenance'
+        verbose_name_plural = 'Vehicle Provenances'
         managed = False
         db_table = 'vehicles_provenances'
 
@@ -282,6 +200,8 @@ class VehiclesStatus(models.Model):
     name = models.CharField(max_length=160, blank=True, null=True)
 
     class Meta:
+        verbose_name = 'Vehicle Status'
+        verbose_name_plural = 'Vehicle Status'
         managed = False
         db_table = 'vehicles_status'
 
--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+from core.models import VehiclesBrands
+
+
+class VehiclesTestCase(TestCase):
+
+    def setUp(self):
+        VehiclesBrands.objects.create(name="BMW")
+
+    def the_name_of_the_brand(self):
+        BMW = VehiclesBrands.objects.get(name="BMW")
+        self.assertEqual(BMW.name, "BMW")
